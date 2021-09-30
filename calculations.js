4361336257ccb373e100ac362f602c82e8c137bb
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-09-30 13:46:15 -0700

    ran working tests

---
+++ calculations.js
@@ -0,0 +1,8 @@
+export function add_two(num){
+    return num + 2;
+}
+
+export function add(num1, num2) {
+    return 5;
+
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -15,4 +16,15 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+    const input1 = 3;
+    const input2 = 2;
+    const expected = 5;
+
+    const actual = add(input1, input2);
+
+    expect.equal(actual, expected);
+
 });
